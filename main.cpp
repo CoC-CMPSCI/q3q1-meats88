7cb2f278dc97dbf8fad033b4c095b830680d5a68
meats88 <pcx883mi@gmail.com>
2026-03-24 21:29:58 -0700

    9:29

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a, b, c, px, mx;
+
+cout << "give A, B, C:";
+cin >> a >> b >> c;
+px = -b + sqrt((b*b) - 4 * a * c)) / 2 * a;
+mx = (-b - sqrt((b*b) - 4 * a * c)) / 2 * a;
+print lRG
+
+
+
+
+
+
+
+
+
+}
