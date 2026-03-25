d5670541fba888691647d917a778736182aff44c
meats88 <pcx883mi@gmail.com>
2026-03-24 21:33:51 -0700

    9:33

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@ int main()
 
 cout << "give A, B, C:";
 cin >> a >> b >> c;
-px = -b + sqrt((b*b) - 4 * a * c)) / 2 * a;
-mx = (-b - sqrt((b*b) - 4 * a * c)) / 2 * a;
-print lRG
+px = (-b + sqrt((b*b) - 4 * a * c)) / (2 * a);
+mx = (-b - sqrt((b*b) - 4 * a * c)) / (2 * a);
+
+cout << setprecision(2);
+cout << px << " " << mx << endl;
 
 
 
